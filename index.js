6d687feef95213211c13ea18241a024dffc5a235
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-25 15:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,27 +26,14 @@ function makeInt(n, base) {
   return parseInt(n, 10)
 }
 
-
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
 
 // var a, b
 // beforeEach(function() {
 //   a = Math.floor(Math.random() * 1000)
 //   b = Math.floor(Math.random() * 1000)
-// })
-
-
-// describe('makeInt(n)', function() {
-//   it('parses n as an integer and returns the parsed integer', function() {
-//     expect(makeInt(a.toString())).toEqual(a)
-//   })
-
-//   it('assumes base 10', function() {
-//     expect(makeInt('0x2328')).toEqual(0)
-//   })
-
-//   it('returns NaN as appropriate', function() {
-//     expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-//   })
 // })
 
 // describe('preserveDecimal(n)', function() {
